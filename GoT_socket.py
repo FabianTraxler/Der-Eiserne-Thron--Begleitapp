2860b64a9eb5e763e248d1ab93b4ccc033a3ede6
Fabian Traxler <h1553958@green5.wu-wien.ac.at>
2018-11-06 15:17:21 +0100

    GoT_socket.py file edited

--- GoT_socket.py
+++ GoT_socket.py
@@ -10,12 +10,12 @@ import socket
 
 class GoT_Socket:
     # IP Adresse des Server mit socket auslesen
-    self.IP = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+    IP = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 
     # Funktionen des Sockets festlegen (Send Messages, Recieve,...)
     def __init__(self, PORT):
         self.app = Flask(__name__)
-        self.socketio = SocketIO(app)
-        self.socketio.run(app, host=self.IP, port=PORT)
+        self.socketio = SocketIO(self.app)
+        self.socketio.run(self.app, host=self.IP, port=PORT)
     
   
