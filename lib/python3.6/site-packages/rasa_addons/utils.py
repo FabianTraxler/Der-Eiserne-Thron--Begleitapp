40ce67f2d98fb7aa2044a0b919bccf108d8ddf29
Fabian <fabi.traxler@hotmail.com>
2018-11-06 14:38:59 +0100

    Revert "Virtual Enviroment added"
    This reverts commit 63887d51f29d3e51bc1cdbee9b9d912acc15c74d.

--- lib/python3.6/site-packages/rasa_addons/utils.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import io
-
-def load_yaml(rules_file):
-    with io.open(rules_file, 'r', encoding='utf-8') as stream:
-        try:
-            return yaml.load(stream)
-        except yaml.YAMLError as exc:
-            raise ValueError(exc)
