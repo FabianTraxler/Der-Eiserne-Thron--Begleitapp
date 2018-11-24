34793b95513e246b39449036f5b0fb8e22061df6
Fabi <fabi.traxler@hotmail.com>
2018-11-24 17:03:23 +0100

    'Updates'

---
+++ startGotServer.bat
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /Users/User/Dektop/Der-Eiserne-Thron--Begleitapp/WebInterface/
+
+python -i app.py
---
+++ startHTTPServer.bat
@@ -0,0 +1,11 @@
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd Users/User/Desktop/Der-Eiserne-Thron--Begleitapp/WebInterface
+python -m http.server 80
